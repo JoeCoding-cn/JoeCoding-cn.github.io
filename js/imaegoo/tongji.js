9758d1800290d3feaa05d8977cd2f285edd5c94d
joejay <1815066657@qq.com>
2020-07-14 16:15:23 +0800

    delete

--- js/imaegoo/tongji.js
+++
@@ -1,8 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?2e971f0f92ab459225ba68504c677f77";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
-
