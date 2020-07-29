7b080c52d65bd3a42591daf82eeca50f9902ce8e
joejay <1815066657@qq.com>
2020-07-29 15:19:20 +0800

    谷歌分析

--- js/imaegoo/tongji.js
+++ js/imaegoo/tongji.js
@@ -20,10 +20,16 @@ var _hmt = _hmt || [];
     s.parentNode.insertBefore(bp, s);
 })();
 
-
+// 谷歌分析 网站追踪
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'UA-173941404-1');
 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+    ga('create', 'UA-173941404-1', 'auto');
+    ga('send', 'pageview');
